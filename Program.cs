2e2adeb2441fc795f28bc28ae5f54aefd747c614
Michael Mathu <mathumichael17@gmail.com>
2022-04-28 20:23:10 +0300

    generic class

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Generic
+{
+    public class PracticeList<T>
+    {
+        private T[] items;
+
+        public PracticeList()
+        {
+            items = new T[0];
+        }
+
+        public T GetItem(int index)
+        {
+            return items[index];
+        }
+
+        public void Add(T newItem)
+        {
+            T[] newItems = new T[items.Length + 1];
+
+            for (int index = 0; index < items.Length; index++)
+                newItems[index] = items[index];
+
+            newItems[items.Length - 1] = newItem;
+
+            items = newItems;
+        }
+    }
+}
